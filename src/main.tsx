eb0061f2ee5977b284c0624ce32e617d066d9184
rentley-app <tech@rentley.app>
2025-09-10 20:56:18 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './app';
+import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -9,3 +9,4 @@ createRoot(document.getElementById('root')!).render(
   </React.StrictMode>
 
 );
+
