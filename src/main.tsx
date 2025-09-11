f0e93ccdec26f28273113f5b56c86062e41f68e4
rentley-app <tech@rentley.app>
2025-09-11 20:28:36 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import SecureWrapper from './SecureWrapper';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "./index.css";
+import MieterPage from "./pages/MieterPage";
+import VermieterPage from "./pages/VermieterPage";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <SecureWrapper>
-      <App />
-    </SecureWrapper>
-  </React.StrictMode>,
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/mieter" replace />} />
+        <Route path="/mieter" element={<MieterPage />} />
+        <Route path="/vermieter" element={<VermieterPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
