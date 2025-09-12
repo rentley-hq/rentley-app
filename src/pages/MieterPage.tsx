ee4ee19d1978db3b6fe2c14d511ee9c0e9b0122f
rentley-app <tech@rentley.app>
2025-09-12 22:29:43 +0200

    Update MieterPage.tsx

--- src/pages/MieterPage.tsx
+++ src/pages/MieterPage.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import NavBar from "../components/NavBar";
+import NavBar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Ratings from "../components/Ratings";
 import ContractCheck from "../components/ContractCheck";
